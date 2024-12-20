773f8fef42d8d1570fbb40e0f5d2962677391173
balramMEL <balrammelkunde1999@gmail.com>
2024-12-20 13:19:25 +0530

    scroller

--- src/components/HorizontalScroller.jsx
+++ src/components/HorizontalScroller.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-
 const HorizontalScroller = () => {
-  const marqueeRef = useRef(null);
-
-  useEffect(() => {
-    const marquee = marqueeRef.current;
-
-    gsap.to(marquee, {
-      x: "-100%", // Moves the text out of view horizontally
-      duration: 20, // Adjust duration for speed
-      ease: "linear",
-      repeat: -1, // Infinite repeat
-    });
-  }, []);
 
   return (
     <div className="bg-red-600 overflow-hidden whitespace-nowrap h-[45px] flex items-center">
       <div
-        ref={marqueeRef}
-        className="text-white uppercase text-sm tracking-wide font-bold flex gap-8"
+        className="text-white uppercase text-sm tracking-wide font-bold flex gap-8 marquee-scroll"
       >
         <span>Progress Tracking</span>
         <span >.</span>
